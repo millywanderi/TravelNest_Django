6f33e19560f33d0edbf41af1b454545ed6b7ca77
millywanderi <wanderinyambura5@gmail.com>
2023-09-13 18:25:43 +0300

    improved home method

--- TravelNest/accounts/views.py
+++ TravelNest/accounts/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Airport, Flight, Train
-
+from . forms import FlightForm
 # Create your views here.
 def home(request):
 
     flights = Flight.objects.all()
 
+    if request.method == "POST":
+        form = FlightForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+        else:
+            form = form
+            return render(request, "accounts/index.html", {"form": form})
+
+    else:
+        form = FlightForm()
+
     context = {
         "flights": flights,
+        "form": form,
     }
     return render(request, "accounts/index.html", context)
