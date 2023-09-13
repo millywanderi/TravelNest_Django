68fcd3a5e16d35968b6693bd23aa571a12a509d3
millywanderi <wanderinyambura5@gmail.com>
2023-09-13 18:23:35 +0300

    updated flight form

--- TravelNest/accounts/forms.py
+++ TravelNest/accounts/forms.py
@@ -1,16 +1,14 @@
 from .models import Flight, Airport
-from django import from 
+from django import forms
 
 class FlightForm(forms.ModelForm):
     class Meta:
         model = Flight
-        Fields = [
-            "arrivalTime",
-            "departureTime",
+        fields = [
             "duration",
             "price",
             "status",
-            "passanger",
+            "passenger",
             "pointOfDeparture",
             "destination"
         ]
